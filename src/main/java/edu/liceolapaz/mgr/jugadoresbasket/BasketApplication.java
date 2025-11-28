9596f9bd3d3ae269c09fbb0f61720066941df6ac
Manuel García <manuel.garcia.rey.48@gmail.com>
2025-11-28 13:17:43 +0100

    primeras modificaciones con errores

--- src/main/java/edu/liceolapaz/mgr/jugadoresbasket/BasketApplication.java
+++ src/main/java/edu/liceolapaz/mgr/jugadoresbasket/BasketApplication.java
@@ -1,17 +1,18 @@
 package edu.liceolapaz.mgr.jugadoresbasket;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class LoginController extends Application {
+public class BasketApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(LoginController.class.getResource("login-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(BasketApplication.class.getResource("login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 307);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
@@ -19,5 +20,9 @@ public class LoginController extends Application {
 
     public static void main(String[] args) {
         launch();
+    }
+
+    public void onLoginButtonClick(ActionEvent actionEvent) {
+
     }
 }
